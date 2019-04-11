204a897d312fc9e1b6d4b79f1afd8428ab8c98df
tracer-ics2018 <tracer@njuics.org>
2019-04-11 11:58:38 +0000

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  11:58:38 up 12:25,  1 user,  load average: 0.01, 0.04, 0.05 4220b71f5e3e7e8e7b106a602500fc2c5c4bb933

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -122,11 +122,13 @@ static bool make_token(char *e) {
 }
 
 uint32_t expr(char *e, bool *success) {
+ /*
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-  
+  */
+    make_token(e);
   /* TODO: Insert codes to evaluate the expression. */
   TODO();
 
