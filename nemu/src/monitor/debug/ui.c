2339597c9571395e8f625cf86bca4105b3a36cf8
tracer-ics2018 <tracer@njuics.org>
2019-04-12 11:54:22 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  11:54:22 up  4:17,  5 users,  load average: 1.57, 1.64, 1.63 deea0808b5f38cd325d5eac0b7611bb848866b5a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -107,7 +107,7 @@ static int cmd_x(char *args){
    // printf("%#lX\n",ad);
     //进行内存扫描,每次四个字节;
     for(int i = 0 ; i < n ; i++){
-        uint32_t data = vaddr_read(addr,4);
+        uint32_t data = vaddr_read(addr + i * 4,4);
         printf("0x%08x  " , addr + i * 4 );
         for(int j =0 ; j < 4 ; j++){
             printf("0x%02x " , data & 0xff);
