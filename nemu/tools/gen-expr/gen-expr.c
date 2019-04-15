8534db6ca240240bbcb358a66748edcdb1ccfeb6
tracer-ics2018 <tracer@njuics.org>
2019-04-15 17:07:15 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  17:07:15 up  9:20,  4 users,  load average: 0.00, 0.04, 0.06 dc7264a85b209e638ff2b0e4221dc3004e919de1

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -20,6 +20,15 @@ static char *code_format =
 "  return 0; "
 "}";
 
+
+void gen_rand_expr(){
+    switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+    }
+}
+
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
