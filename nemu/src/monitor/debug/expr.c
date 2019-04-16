b621a90bf714d476823373c9d30f8878e843ca98
tracer-ics2018 <tracer@njuics.org>
2019-04-16 14:47:11 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  14:47:11 up  7:02,  4 users,  load average: 0.27, 0.10, 0.07 7e2f4dc2641641abb46d0d99dd2a37187e86b25

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -248,10 +248,11 @@ uint32_t eval(int p ,int  q) {
     else {
             /* We should do more things here. */
         op = dominant_operator(p,q);
-        printf("op位置是 %d \n",op);
+        // printf("op位置是 %d \n",op);
         //处理负号和指针 优先级为2 
         if(tokens[op].type == TK_NEG){
             sscanf(tokens[op+1].str, "%x", &result);
+            printf("---%d \n",result);
             return -result;
         }
         else if (tokens[op].type == TK_POINT){
