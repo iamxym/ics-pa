22a687bad808140307d1b52aee3695b79b270558
tracer-ics2018 <tracer@njuics.org>
2019-04-17 23:29:28 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  23:29:28 up 14:39,  4 users,  load average: 0.29, 0.10, 0.07 5aacd5d1395affb21bca5c945aaa9a79c363dc17

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -7,7 +7,7 @@ typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
   uint32_t value; //最近一次的数值；
-  char *expr; //记录监视的表达式。
+  char expr[65536]; //记录监视的表达式。
   int type; //类型，断点和监视点；
   bool isused; //监视点是否使用。
   /* TODO: Add more members if necessary */
