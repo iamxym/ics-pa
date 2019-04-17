959734a9913388b55099dd0970b8128eda0430db
tracer-ics2018 <tracer@njuics.org>
2019-04-17 21:45:03 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  21:45:03 up 12:54,  4 users,  load average: 0.04, 0.05, 0.05 d536d9c679bf36a18dc3421c7f613109705a918d

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,8 +6,10 @@
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-  uint32_t lastVal; //最近一次的数值；
+  uint32_t value; //最近一次的数值；
   char *expr; //记录监视的表达式。
+  int type; //类型，断点和监视点；
+  bool isused; //监视点是否使用。
   /* TODO: Add more members if necessary */
 
 
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,22 +1,29 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
-
+#include <stdlib.h>
+#include <string.h>
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
 
-
+//清空某监视点的下一项指针
 void clearWP(WP *wp);
 
+//获取 head 
 WP *getHeadWP();
 
+//申请使用一个新监视点（内部调用 new_wp() 并更新链表信息）
 WP *createWP();
 
+//删除指定编号的监视点
 void removeWP(int no);
 
+
+//（私有）从内存池中获取下一个能使用的监视点，并作一定预处理
 WP *new_wp();
 
+//（私有）释放一个监视点
 void free_wp(WP *wp);
 
 void init_wp_pool() {
@@ -32,5 +39,27 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+//需要存入表达式和结果。
+WP *new_wp(char *str , int value){
+    if(free_ == NULL){
+        printf("Erro!free is null.\n");
+        assert(0);
+    }
+    WP *new = NULL;
+    new = free_;
+    free_ = free_->next;
+    new->value = value;
+    strcpy(new->expr, str);
+    new->next = NULL;
+    new->isused = true;
+    if(head == NULL) head = new;
+    else{
+        new->next = head;
+        head = new ;
+    }
+    return new;
+}
+
+
 
 
