befe3d70f47fa1fdbacfe1282d5281e82bbe0f21
tracer-ics2018 <tracer@njuics.org>
2019-04-19 07:54:41 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  07:54:41 up 26 min,  4 users,  load average: 0.01, 0.10, 0.12 d82c7bd21fac891d6860227a8f4f0df12249d257

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -247,16 +247,16 @@ uint32_t eval(int p ,int  q) {
             t =  strtol(tokens[p].str,&str,16 );
             return t;
         case TK_REG :
-            printf("%s \n", tokens[p].str);
-            if(strcmp(tokens[p].str,"eax") == 0) return cpu.eax;
-            else if(strcmp(tokens[p].str ,"ecx") == 0) return cpu.ecx;
-            else if(strcmp(tokens[p].str ,"edx") == 0) return cpu.edx;
-            else if(strcmp(tokens[p].str ,"ebx") == 0) return cpu.ebx;
-            else if(strcmp(tokens[p].str ,"esp") == 0) return cpu.esp;
-            else if(strcmp(tokens[p].str ,"ebp") == 0) return cpu.ebp;
-            else if(strcmp(tokens[p].str ,"esi") == 0) return cpu.esi;
-            else if(strcmp(tokens[p].str ,"edi") == 0) return cpu.edi;
-            else if(strcmp(tokens[p].str ,"eip") == 0) return cpu.eip;
+           // printf("%s \n", tokens[p].str);
+            if(strcmp(tokens[p].str,"$eax") == 0) return cpu.eax;
+            else if(strcmp(tokens[p].str ,"$ecx") == 0) return cpu.ecx;
+            else if(strcmp(tokens[p].str ,"$edx") == 0) return cpu.edx;
+            else if(strcmp(tokens[p].str ,"$ebx") == 0) return cpu.ebx;
+            else if(strcmp(tokens[p].str ,"$esp") == 0) return cpu.esp;
+            else if(strcmp(tokens[p].str ,"$ebp") == 0) return cpu.ebp;
+            else if(strcmp(tokens[p].str ,"$esi") == 0) return cpu.esi;
+            else if(strcmp(tokens[p].str ,"$edi") == 0) return cpu.edi;
+            else if(strcmp(tokens[p].str ,"$eip") == 0) return cpu.eip;
             else  assert(0);
         default :   assert(0);
         }
