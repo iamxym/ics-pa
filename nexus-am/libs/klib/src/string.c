b97679142a407de422a6ce849cf9ba4b49a75a4a
DuskwatcherLT <719899089@qq.com>
2019-04-24 15:14:50 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  15:14:50 up  6:54,  4 users,  load average: 0.24, 0.09, 0.06 c3182e9dd75d762fc74908dd2b73fa014dfe0947

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,82 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+    size_t i = 0;
+    while(*(s+i) != '\0') i++;
+    return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+    size_t i = 0;
+    while(*(src+i) != '\0'){
+        *(dst+i) = *(src+i);
+        i++;
+    }
+    //注意此处不需要'\0'
+    return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+    size_t i;
+    for(i = 0; i < n && src[i] != '\0'; ++i)    dst[i] = src[i];                  
+    for( ; i < n; ++i )  dst[i] = '\0';
+    return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+    size_t dst_len = strlen(dst);
+    size_t i = 0;
+    for(i = 0; src[i] != '\0'; ++i) dst[dst_len + i] = src[i];
+    dst[dst_len + i] = '\0';
+    return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+    int l1 = strlen(s1);
+    int l2 = strlen(s2);
+    int l = 0;
+    if(l1 < l2) l = l1;
+    else l = l2;
+    for(int i = 0; i <= l; ++i){
+        if(s1[i] < s2[i]) return -1;
+        else if(s1[i] > s2[i]) return 1;
+    }
+    return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+    for(size_t i = 0; i < n; ++i){
+        if(s1[i] < s2[i]) return -1;
+        else if(s1[i] > s2[i]) return 1;                      
+    }
+    return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+    if(v == NULL || n < 0) return NULL;
+    char* temp = (char*) v;
+    for(int i = 0; i < n; ++i){
+        temp[i] = (char) c;
+    }
+    return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+    if(out == NULL || n < 0) return NULL;
+    char* charout = (char*) out;
+    char* charin = (char*) in;
+    for(int i = 0; i < n; ++i)    charout[i] = charin[i];
+    return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+    char* c1 = (char*) s1;
+    char* c2 = (char*) s2;
+    for(int i = 0; i < n; ++i){
+        if(c1[i] < c2[i]) return -1;
+        else if(c1[i] > c2[i]) return 1;
+    }
+    return 0;
 }
 
 #endif
