67d190ec27383fb884812acc4ff78c700becec66
DuskwatcherLT <719899089@qq.com>
2019-04-25 16:38:16 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  16:38:16 up 1 day,  8:18,  4 users,  load average: 1.30, 0.89, 0.47 7e8d7e17eb48d7451e6faee18a90c6164d5366dd

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,6 +1,13 @@
 #include "nemu.h"
+#include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
+#define VMEM_SIZE 0xffffffff
+#define PRESENT(x) (x & 0x00000001)
+#define HIGH20(x) ((x & 0xfffff000) >> 12)
+#define PG 0x80000000
+#define PGSIZE 0x1000
 
 #define pmem_rw(addr, type) *(type *)({\
     Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
@@ -9,20 +16,95 @@
 
 uint8_t pmem[PMEM_SIZE];
 
+paddr_t page_translate(vaddr_t addr) {
+	CR0 pt_cr0;
+	pt_cr0.val = cpu.cr0;
+	if(pt_cr0.paging == 0) return addr;
+
+	CR3 pt_cr3;
+	pt_cr3.val = cpu.cr3;
+	//Log("CR3 = 0x%x", pt_cr3.val);
+	uint32_t pt_dir_base = pt_cr3.page_directory_base << 12;
+	//Log("pt_dir_base is at 0x%x",pt_dir_base);
+	
+	PDE pt_pde;
+	uint32_t dir = addr >> 22;
+	pt_pde.val = paddr_read(pt_dir_base + 4 * dir , 4);
+	//Log("pt_pde is at 0x%x", pt_dir_base + 4 * dir);
+	//Log("PDE.val = 0x%x", pt_pde.val);
+	Assert(pt_pde.present, "invalid PDE value when reading 0x%x", addr);
+
+	PTE pt_pte;
+	uint32_t page = (addr & 0x003ff000) >> 12;
+	pt_pte.val = paddr_read((pt_pde.page_frame << 12) + 4 * page, 4);
+	Assert(pt_pte.present, "invalid PTE value when reading 0x%x", addr);
+
+	uint32_t offset = addr & 0x00000fff;
+	paddr_t paddr = (pt_pte.page_frame << 12) + offset;
+	//Log("paddr = 0x%x", paddr);
+	return paddr;
+}
+
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+	//int mm = is_mmio(addr);
+	static int mm;
+	mm = is_mmio(addr);
+	if(mm == -1)
+  	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+	else return mmio_read(addr, len, mm);
 }
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
-  memcpy(guest_to_host(addr), &data, len);
+	//int mm = is_mmio(addr);
+	static int mm;
+	mm = is_mmio(addr);
+	if(mm == -1)
+  	memcpy(guest_to_host(addr), &data, len);
+	else  mmio_write(addr, len, data, mm);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+	if((addr & 0xfff) + len > PGSIZE) {
+		//assert(0);
+		//Log("vaddr=0x%x, len=%d", addr, len);
+		int this_page_len = PGSIZE - (addr & 0xfff);
+		paddr_t paddr = page_translate(addr);
+		uint32_t this_page = paddr_read(paddr, this_page_len);
+		paddr = page_translate(addr + this_page_len);
+		int next_page_len = len - this_page_len;
+		uint32_t next_page = paddr_read(paddr, next_page_len);
+		uint32_t real_data = (next_page << (this_page_len * 8)) + this_page;
+		return real_data;
+	}// data cross the page boundary
+	else {
+		paddr_t paddr = page_translate(addr);
+  	return paddr_read(paddr, len);
+	}
 }
 
 void vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+	if((addr & 0xfff) + len > PGSIZE) {
+		//assert(0);
+		int this_page_len = PGSIZE -(addr & 0xfff);
+		paddr_t paddr = page_translate(addr);
+		int this_page = data;
+		switch(this_page_len){
+			case 1: this_page = data & 0xff; break;
+			case 2: this_page = data & 0xffff; break;
+			case 3: this_page = data & 0xffffff; break;
+			case 4: this_page = data; break;
+			default: assert(0);
+		}
+		paddr_write(paddr, this_page, this_page_len);
+		int next_page_len = len - this_page_len;
+		int next_page = data - this_page;
+		paddr = page_translate(addr + this_page_len);
+		paddr_write(paddr, next_page, next_page_len);
+	}
+	else {
+		paddr_t paddr = page_translate(addr);			
+ 	  paddr_write(paddr, data, len);
+	}
 }
