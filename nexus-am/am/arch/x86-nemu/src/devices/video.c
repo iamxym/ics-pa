aac56d827c6dc5a4640d9eeee3171bc911ad4fa9
DuskwatcherLT <719899089@qq.com>
2019-04-26 11:33:08 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  11:33:08 up 2 days,  3:13,  4 users,  load average: 0.07, 0.04, 0.12 2f4d8c5ddba2041486bd53829f687882731762c8

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,7 +3,18 @@
 #include <amdev.h>
 #include <klib.h>
 
+#define SCREEN_PORT 0x100
+#define SCREEN_H 300
+#define SCREEN_W 400
+
+
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+
+int min(int a, int b){
+    if(a <= b) return a;
+    else return b;            
+}
+
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -21,6 +32,13 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * min(w, SCREEN_W - x);
+      for(int j = 0; j < h  && y + j < SCREEN_H; ++j){
+          memcpy(&fb[(y + j) * SCREEN_W + x], pixels, cp_bytes);
+          pixels += w;
+      }
 
       if (ctl->sync) {
         // do nothing, hardware syncs.
