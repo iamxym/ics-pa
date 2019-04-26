82a2dd262a5f59f0e12befec0af37f5408f6d0f2
DuskwatcherLT <719899089@qq.com>
2019-04-26 10:59:49 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  10:59:49 up 2 days,  2:39,  4 users,  load average: 0.13, 0.82, 0.84 ec69e15b29bb41f95f153945b9d3b80abab5c94

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -3,10 +3,10 @@ void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+//  int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
-  ui_mainloop(is_batch_mode);
+//  ui_mainloop(is_batch_mode);
 
   return 0;
 }
