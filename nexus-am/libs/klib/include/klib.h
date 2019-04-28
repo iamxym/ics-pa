03c45fa68b3eda1ad06e2dce30227f286d098f1d
DuskwatcherLT <719899089@qq.com>
2019-04-28 22:47:06 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  22:47:06 up  6:35,  4 users,  load average: 0.05, 0.04, 0.05 8f9efbf93e04a8fd4d8e9393a174a8b615020a9a

--- nexus-am/libs/klib/include/klib.h
+++ nexus-am/libs/klib/include/klib.h
@@ -61,6 +61,7 @@ int sprintf(char* out, const char* format, ...);
 int snprintf(char* s, size_t n, const char* format, ...);
 int vsprintf(char *str, const char *format, va_list ap);
 
+#define printk printf
 // assert.h
 #ifdef NDEBUG
   #define assert(ignore) ((void)0)
