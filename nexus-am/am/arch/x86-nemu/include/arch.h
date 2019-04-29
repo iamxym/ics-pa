af924ad68a94f6815c3437d5d2276f9144673161
DuskwatcherLT <719899089@qq.com>
2019-04-29 14:18:43 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  14:18:43 up 22:07,  4 users,  load average: 0.00, 0.01, 0.05 f7239e3148829e0b4733b7594a59d922e931188

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -12,7 +12,8 @@ struct _Context {
  // uintptr_t esi, ebx, eax, eip, edx, err, eflags, ecx, cs, esp, edi, ebp;
   struct _Protect *prot;
   //first is push.
-  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  //uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  uintptr_t eax,ecs,edx,ebx,esp,ebp,esi,edi;
   int       irq;
   uintptr_t err, eip, cs, eflags;
 };
