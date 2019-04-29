d86c3026f901241d868ef2806d8493d3e53fb860
DuskwatcherLT <719899089@qq.com>
2019-04-29 16:44:15 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  16:44:15 up 1 day, 32 min,  4 users,  load average: 0.10, 0.04, 0.05 4900067e9fd288bcc4d5aaadd5ec72c21b9d027

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,9 +2,23 @@
 
 #define DEFAULT_ENTRY 0x4000000
 
+typedef size_t ssize_t;
+typedef size_t off_t;
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_filesz(int fd);
+ssize_t fs_read(int fd, void* buf, size_t len);
+ssize_t fs_close(int fd);
+ssize_t fs_write(int fd, const void* buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return DEFAULT_ENTRY;
+  //TODO();
+  int fd = fs_open(filename, 0, 0);
+    fs_read(fd, (void*)DEFAULT_ENTRY, fs_filesz(fd));
+        fs_close(fd);
+    return DEFAULT_ENTRY;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
