153a890a56d9910ee0753a90cf6e5c5f94aaac6a
DuskwatcherLT <719899089@qq.com>
2019-04-29 09:43:22 +0800

    >  compile 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  09:43:22 up 17:31,  4 users,  load average: 0.04, 0.03, 0.05 be24b04cd9ae0cd57c1f86d148458fc14b19c818

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,15 +11,14 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
     rtl_push(&ret_addr);
 
        
-    GateDesc gate_des;
-    gate_des.val1 = vaddr_read(cpu.idtr.base + NO * 8, 4);
-    gate_des.val2 = vaddr_read(cpu.idtr.base + NO * 8 + 4, 4);
-    if(gate_des.present == 0){
-        panic("Invalid gate descriptor!");
+    GateDesc gate_desc;
+    gate_desc.val1 = vaddr_read(cpu.idtr.base + NO * 8, 4);
+    gate_desc.val2 = vaddr_read(cpu.idtr.base + NO * 8 + 4, 4);
+    if(gate_desc.present == 0){
+        panic("Invalid!");
         assert(0);
-                }
-    vaddr_t aim = (gate_des.val1 & 0x0000ffff) + (gate_des.val2 & 0xffff0000);
-    printf("!!!!!!\n");
+    }
+    vaddr_t aim = (gate_desc.val1 & 0x0000ffff) + (gate_desc.val2 & 0xffff0000);
     rtl_j(aim);
     // TODO();
 }
