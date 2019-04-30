6cc9b56cd69ecdf4d2a180ccdc0fd14c4dc8b790
DuskwatcherLT <719899089@qq.com>
2019-04-30 08:56:11 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  08:56:11 up 1 day, 16:44,  4 users,  load average: 0.00, 0.02, 0.05 b83af9e6f664f7529978108575b75cf264e2e5c5

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,17 @@
 #include "common.h"
 #include "syscall.h"
+#include "proc.h"
 
 _Context* do_syscall(_Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
+  	uintptr_t a[4];
+  	a[0] = c->GPR1;
+	a[1] = c->GPR2;	//parameter1:status
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  case SYS_yield : _yield();c->GPRx = 0 ;break;
+  default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
