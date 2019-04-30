7289aaac0702a1c586c050be027ad34d3c7e7a97
DuskwatcherLT <719899089@qq.com>
2019-04-30 09:12:07 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  09:12:07 up 1 day, 17:00,  4 users,  load average: 0.04, 0.08, 0.06 eb2d9dc4d7ab8e0aa48006f88a097e3b19d7e091

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,6 +11,7 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
   case SYS_yield : _yield();c->GPRx = 0 ;break;
+  case SYS_exit : _halt(a[1]);break;
   default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
