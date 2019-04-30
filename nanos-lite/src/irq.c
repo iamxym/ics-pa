060ea97a992737d9ea6d2f7a14648bdfaef74a35
DuskwatcherLT <719899089@qq.com>
2019-04-30 08:23:46 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  08:23:46 up 1 day, 16:12,  4 users,  load average: 0.11, 0.07, 0.06 745f25d0f02788bfd8a818ffbad618423bfb3bb0

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ _Context* do_syscall(_Context *c);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
         case _EVENT_YIELD: Log("ID = %d: Recognize yield event!",e.event); break;                   
-        case _EVENT_SYSCALL: Log("ID = %d: Recognize syscall event!",e.event);break; 
+        case _EVENT_SYSCALL: do_syscall(c);break; 
 
   default: panic("Unhandled event ID = %d", e.event);
   }
