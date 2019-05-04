2db318178daa5c8d239d02d22eee6d80d1b1ae6c
DuskwatcherLT <719899089@qq.com>
2019-05-04 22:52:45 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  22:52:45 up  9:57,  4 users,  load average: 0.26, 0.11, 0.07 6e763a4b7f882335b116a1da585c9186aa6f60

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -26,6 +26,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
   case SYS_yield :  _yield();c->GPRx = 0 ;break;
   case SYS_exit :   _halt(a[2]);break;
+  case SYS_kill : Log("!!!!!");break;
   case SYS_write :	Log("used sys_write!");
 				 c->GPRx = (ssize_t)fs_write((int)a[1], (const void*)a[2], (size_t)a[3]);break;
   case SYS_brk :	Log("Used sys_brk end=%d!",&_end);
