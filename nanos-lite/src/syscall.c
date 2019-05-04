29fa33f478f2547d256c5aa2c0a7dcfb97ade0d0
DuskwatcherLT <719899089@qq.com>
2019-05-04 21:27:35 +0800

    >  run 20190405 root Linux 25cc4548724d 3.10.0-957.10.1.el7.x86_64 #1 SMP Mon Mar 18 15:06:45 UTC 2019 x86_64 GNU/Linux  21:27:35 up  8:32,  4 users,  load average: 0.03, 0.04, 0.05 e17162adef9dd705ce3632796ef63c3e17d9c7b8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -25,9 +25,21 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
   case SYS_yield : _yield();c->GPRx = 0 ;break;
   case SYS_exit : _halt(a[2]);break;
-  case SYS_write:Log("used sys_write!"); c->GPRx = (ssize_t)fs_write((int)a[1], (const void*)a[2], (size_t)a[3]);break;
-  case SYS_brk :Log("Used sys_brk"); _heap.end = (void *)a[1]; c->GPRx = 0 ;break;
-  default: panic("Unhandled syscall ID = %d", a[0]);
+  case SYS_write:	Log("used sys_write!");
+				 c->GPRx = (ssize_t)fs_write((int)a[1], (const void*)a[2], (size_t)a[3]);break;
+  case SYS_brk :	Log("Used sys_brk!");
+				 _heap.end = (void *)a[1]; c->GPRx = 0 ;break;
+  case SYS_read: 	Log("Used sys_read! len = %d", a[3]);
+				 c->GPRx = (ssize_t)fs_read((int)a[1], (void*)a[2], (size_t)a[3]);break;
+  case SYS_open: 	Log("Used sys_open!");
+   			 	c->GPRx = (ssize_t)fs_open((const char*)a[1], (int)a[2], (int)a[3]);break;
+  case SYS_close:   Log("Used sys_close!");
+    			c->GPRx = (ssize_t)fs_close((int)a[1]);break;
+  case SYS_lseek:  	Log("Usde sys_lseek!");
+     			c->GPRx = (off_t)fs_lseek((int)a[1], (off_t)a[2], (int)a[3]);break;	
+  case SYS_execve: Log("Usde SYS_execve, name = %s", (char*)a[1]);
+ 					 naive_uload(NULL, (char*)a[1]);c->GPRx = 0;break;  
+default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
